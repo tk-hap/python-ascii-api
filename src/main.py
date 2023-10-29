a4ea1a30b941e2d7ba672d2a7fb71f0838f3b3fa
Te Kaanu Hapuku <34885571+tk-hap@users.noreply.github.com>
2023-10-30 08:18:19 +1300

    create basic routes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+from models import ASCII_Art
 
 ascii_api = FastAPI()
+
+art_dict = []
+
 
 @ascii_api.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@ascii_api.get("/art")
+async def get_art():
+    return art_dict
+
+@ascii_api.post("/art")
+async def add_art():
+    return {"message": "Art added successfully"}
+
+@ascii_api.get("/art/{art_id}", response_class=PlainTextResponse)
+async def read_art(art_id: str) -> str:
+    return art_dict[art_id]
