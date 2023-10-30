730c9c8e05c1e013aac5a52e4a30cd7beaa5ff5e
TK Hapuku <tekaanuh@gmail.com>
2023-10-31 09:34:40 +1300

    switch from storing data in list to redis

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
+import redis
 from models import ASCII_Art
 
 ascii_api = FastAPI()
 
-ascii_art = []
-
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 @ascii_api.get("/")
 async def root():
@@ -14,13 +14,13 @@ async def root():
 
 @ascii_api.get("/art")
 async def get_art():
-    return { "ascii_art": ascii_art}
+    return {"ascii_art": r.keys()}
 
 @ascii_api.post("/art")
 async def add_art(art: ASCII_Art):
-    ascii_art.append(art)
+    r.set(art.name, art.ascii)
     return {"message": "Art added successfully"}
 
 @ascii_api.get("/art/{art_id}", response_class=PlainTextResponse)
-async def read_art(art_id: str) -> str:
-    return ascii_art[art_id]
+async def read_art(art_id: str):
+    return r.get(art_id)
